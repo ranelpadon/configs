0dbecb8a3f5901a35fe4a370bfbbe18655fb3dee
Ranel Padon <ranel.padon@gmail.com>
2021-04-24 18:22:09 +0800

    Update the tmuxrc.sh file.

--- tmuxrc.sh
+++ tmuxrc.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 # Tabs
-tmux new-session -s Workspace -n "Nvim ETS" -d
+tmux new-window -s Workspace -n "Configs" -d
+tmux new-session -t Workspace -n "Nvim ETS"
 tmux new-window -t Workspace -n "ETS"
 tmux new-window -t Workspace -n "Docker"
 tmux new-window -t Workspace -n "Dev"
-tmux new-window -t Workspace -n "Configs"
 tmux new-window -t Workspace -n "Home"
 
 # Auto-runs
+tmux send-keys -t "Workspace:Configs" "cd ~/dev/configs" Enter
 tmux send-keys -t "Workspace:Nvim ETS" "cd ~/dev/ticketflap/ticketing && pyenv activate ticketing && nvim" Enter
 tmux send-keys -t "Workspace:ETS" "cd ~/dev/ticketflap/ticketing" Enter
 tmux send-keys -t "Workspace:Docker" "cd ~/dev/ticketflap/ticketing/docker" Enter
-tmux send-keys -t "Workspace:Configs" "cd ~/dev/configs" Enter
 tmux send-keys -t "Workspace:Dev" "cd ~/dev" Enter
 
 # Activate main window
-tmux select-window -t "Workspace:Nvim ETS"
+tmux select-window -t "Workspace:configs"
 tmux -u attach-session -t Workspace
