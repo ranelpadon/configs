a7cdef47d97772cbeb7ddaad59631e03765856d3
Ranel Padon <ranel.padon@gmail.com>
2021-07-31 09:47:10 +0800

    Update the Tmux script.

--- tmuxrc.sh
+++ tmuxrc.sh
@@ -2,6 +2,7 @@
 
 # Tabs
 tmux new-session -s Workspace -n "Configs" -d
+tmux new-window -t Workspace -n "Vortex"
 tmux new-window -t Workspace -n "Nvim ETS"
 tmux new-window -t Workspace -n "ETS"
 tmux new-window -t Workspace -n "Docker"
@@ -10,6 +11,7 @@ tmux new-window -t Workspace -n "Home"
 
 # Auto-runs
 tmux send-keys -t "Workspace:Configs" "cd ~/dev/configs && nvim" Enter
+tmux send-keys -t "Workspace:Vortex" "cd ~/Dropbox/Vortex && nvim" Enter
 tmux send-keys -t "Workspace:Nvim ETS" "cd ~/dev/ticketflap/ticketing && pyenv activate ticketing && nvim" Enter
 tmux send-keys -t "Workspace:ETS" "cd ~/dev/ticketflap/ticketing" Enter
 tmux send-keys -t "Workspace:Docker" "cd ~/dev/ticketflap/ticketing/docker" Enter
