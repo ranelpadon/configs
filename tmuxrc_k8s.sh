7452625cdd9758ac3c5b97b5a16b0b86a9d8bc53
Ranel Padon <ranel.padon@gmail.com>
2021-08-05 00:54:01 +0800

    Integrate auto-opening of browser links.

--- tmuxrc_k8s.sh
+++ tmuxrc_k8s.sh
@@ -10,13 +10,55 @@ tmux new-window -t k8s -n 'HKRUSTAG'
 tmux new-window -t k8s -n 'ZKTSTAG'
 
 # Auto-runs
-tmux send-keys -t 'k8s:MCSTAG' 'cd ~/dev/melco && gc release/production && gpl && nvim' Enter
-tmux send-keys -t 'k8s:MGMSTAG' 'cd ~/dev/mgm && gc main && gpl && nvim' Enter
-tmux send-keys -t 'k8s:GTSSTAG' 'cd ~/dev/11-skies && gc main && gpl && nvim' Enter
-tmux send-keys -t 'k8s:ZIPSTAG' 'cd ~/dev/zipcity && gc main && gpl && nvim' Enter
-tmux send-keys -t 'k8s:SUNSTAG' 'cd ~/dev/sun-entertainment && gc main && gpl && nvim' Enter
-tmux send-keys -t 'k8s:HKRUSTAG' 'cd ~/dev/hkru && gc main && gpl && nvim' Enter
-tmux send-keys -t 'k8s:ZKTSTAG' 'cd ~/dev/zicket && gc main && gpl && nvim' Enter
+tmux send-keys -t 'k8s:MCSTAG' '
+    open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/melco/-/pipelines" &&
+    cd ~/dev/melco &&
+    gc release/production &&
+    gpl &&
+    nvim
+' Enter
+tmux send-keys -t 'k8s:MGMSTAG' '
+    open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/mgm/-/pipelines" &&
+    cd ~/dev/mgm &&
+    gc main &&
+    gpl &&
+    nvim
+' Enter
+tmux send-keys -t 'k8s:GTSSTAG' '
+    open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/11-skies/-/pipelines" &&
+    cd ~/dev/11-skies &&
+    gc main &&
+    gpl &&
+    nvim
+' Enter
+tmux send-keys -t 'k8s:ZIPSTAG' '
+    open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/zipcity/-/pipelines" &&
+    cd ~/dev/zipcity &&
+    gc main &&
+    gpl &&
+    nvim
+' Enter
+tmux send-keys -t 'k8s:SUNSTAG' '
+    open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/sun-entertainment/-/pipelines" &&
+    cd ~/dev/sun-entertainment &&
+    gc main &&
+    gpl &&
+    nvim
+' Enter
+tmux send-keys -t 'k8s:HKRUSTAG' '
+    open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/hkru/-/pipelines" &&
+    cd ~/dev/hkru &&
+    gc main &&
+    gpl &&
+    nvim
+' Enter
+tmux send-keys -t 'k8s:ZKTSTAG' '
+    open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/zicket/-/pipelines" &&
+    cd ~/dev/zicket &&
+    gc main &&
+    gpl &&
+    nvim
+' Enter
 
 # Activate main window
 tmux select-window -t 'k8s:MCSTAG'
