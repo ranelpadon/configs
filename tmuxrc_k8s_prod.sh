4a7a7f2a794553b1116a5e9940eeaba73ee571d9
Ranel Padon <ranel.padon@gmail.com>
2021-08-23 23:27:54 +0800

    Create tmux scripts for STAG/PROD deployments.

---
+++ tmuxrc_k8s_prod.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# Run as in (new Alacritty window or in Kitty):
+# kube_prod ets-prod-v1.8.47-RC123
+# since $TAG is needed envvar.
+
+# Tabs
+tmux new-session -s k8s_prod -n 'DEMOSTAG' -d
+tmux new-window -t k8s_prod -n 'GTSPROD'
+tmux new-window -t k8s_prod -n 'KGGPROD'
+tmux new-window -t k8s_prod -n 'MGMPROD'
+tmux new-window -t k8s_prod -n 'ZIPPROD'
+
+# Auto-runs
+tmux send-keys -t 'k8s_prod:DEMOSTAG' '
+    open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/demo/-/pipelines" &&
+    cd ~/dev/demo &&
+    gc main &&
+    gpl &&
+    fd --regex "gitlab-ci.yml|gitlab-ci.production.*" --hidden --print0 | xargs -0 sed -i "" -e "s/ets-prod-v1.8.47-RC[0-9]\{1,\}$/$TAG/" &&
+    gd
+' Enter
+tmux send-keys -t 'k8s_prod:GTSPROD' '
+    open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/11-skies/-/pipelines" &&
+    cd "~/dev/11-skies" &&
+    gc main &&
+    gpl &&
+    git tag $TAG &&
+    git push origin $TAG
+' Enter
+tmux send-keys -t 'k8s_prod:KGGPROD' '
+    open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/kgg-kg/-/pipelines" &&
+    cd ~/dev/kgg-kg &&
+    gc main &&
+    gpl &&
+    fd --regex "gitlab-ci.yml|gitlab-ci.production.*" --hidden --print0 | xargs -0 sed -i "" -e "s/ets-prod-v1.8.47-RC[0-9]\{1,\}$/$TAG/" &&
+    gd
+' Enter
+tmux send-keys -t 'k8s_prod:MGMPROD' '
+    open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/mgm/-/pipelines" &&
+    cd ~/dev/mgm &&
+    gc main &&
+    gpl &&
+    fd --regex "gitlab-ci.yml|gitlab-ci.production-shared.*" --hidden --print0 | xargs -0 sed -i "" -e "s/ets-prod-v1.8.47-RC[0-9]\{1,\}$/$TAG/" &&
+    gd
+' Enter
+tmux send-keys -t 'k8s_prod:ZIPPROD' '
+open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/zipcity/-/pipelines" &&
+    cd ~/dev/zipcity &&
+    gc main &&
+    gpl &&
+    git tag $TAG &&
+    git push origin $TAG
+' Enter
+
+# Activate main window
+tmux select-window -t 'k8s_prod:DEMOSTAG'
+tmux -u attach-session -t k8s_prod
--- tmuxrc_k8s_stag.sh
+++ tmuxrc_k8s_stag.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 # Run as in (new Alacritty window or in Kitty):
-# kube ets-prod-v1.8.47-RC123
+# kube_stag ets-prod-v1.8.47-RC123
 # since $TAG is needed envvar.
 
 # Tabs
-# tmux new-session -s k8s -n 'DEMOSTAG' -d
-tmux new-session -s k8s -n 'GTSSTAG' -d
-tmux new-window -t k8s -n 'HKRUSTAG'
-tmux new-window -t k8s -n 'MCSTAG'
-tmux new-window -t k8s -n 'MGMSTAG'
-tmux new-window -t k8s -n 'SUNSTAG'
-tmux new-window -t k8s -n 'ZIPSTAG'
-tmux new-window -t k8s -n 'ZKTSTAG'
+# tmux new-session -s k8s_stag -n 'DEMOSTAG' -d
+tmux new-session -s k8s_stag -n 'GTSSTAG' -d
+tmux new-window -t k8s_stag -n 'HKRUSTAG'
+tmux new-window -t k8s_stag -n 'MCSTAG'
+tmux new-window -t k8s_stag -n 'MGMSTAG'
+tmux new-window -t k8s_stag -n 'SUNSTAG'
+tmux new-window -t k8s_stag -n 'ZIPSTAG'
+tmux new-window -t k8s_stag -n 'ZKTSTAG'
 
 # Auto-runs
-# tmux send-keys -t 'k8s:DEMOSTAG' '
+# tmux send-keys -t 'k8s_stag :DEMOSTAG' '
     # open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/demo/-/pipelines" &&
     # cd ~/dev/demo &&
     # gc main &&
@@ -23,7 +23,7 @@ tmux new-window -t k8s -n 'ZKTSTAG'
     # fd ".gitlab*" --hidden --print0 | xargs -0 sed -i "" -e "s/ets-prod-v1.8.47-RC[0-9]\{1,\}$/$TAG/" &&
     # gd
 # ' Enter
-tmux send-keys -t 'k8s:GTSSTAG' '
+tmux send-keys -t 'k8s_stag :GTSSTAG' '
     open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/11-skies/-/pipelines" &&
     cd ~/dev/11-skies &&
     gc main &&
@@ -31,7 +31,7 @@ tmux send-keys -t 'k8s:GTSSTAG' '
     fd --regex "gitlab-ci.yml|gitlab-ci.staging.*" --hidden --print0 | xargs -0 sed -i "" -e "s/ets-prod-v1.8.47-RC[0-9]\{1,\}$/$TAG/" &&
     gd
 ' Enter
-tmux send-keys -t 'k8s:HKRUSTAG' '
+tmux send-keys -t 'k8s_stag :HKRUSTAG' '
     open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/hkru/-/pipelines" &&
     cd ~/dev/hkru &&
     gc main &&
@@ -39,7 +39,7 @@ tmux send-keys -t 'k8s:HKRUSTAG' '
     fd "gitlab*" --hidden --print0 | xargs -0 sed -i "" -e "s/ets-prod-v1.8.47-RC[0-9]\{1,\}$/$TAG/" &&
     gd
 ' Enter
-tmux send-keys -t 'k8s:MCSTAG' '
+tmux send-keys -t 'k8s_stag :MCSTAG' '
     open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/melco/-/pipelines" &&
     cd ~/dev/melco &&
     gc release/production &&
@@ -47,7 +47,7 @@ tmux send-keys -t 'k8s:MCSTAG' '
     fd "gitlab*" --hidden --print0 | xargs -0 sed -i "" -e "s/ets-prod-v1.8.47-RC[0-9]\{1,\}$/$TAG/" &&
     gd
 ' Enter
-tmux send-keys -t 'k8s:MGMSTAG' '
+tmux send-keys -t 'k8s_stag :MGMSTAG' '
     open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/mgm/-/pipelines" &&
     cd ~/dev/mgm &&
     gc main &&
@@ -55,7 +55,7 @@ tmux send-keys -t 'k8s:MGMSTAG' '
     fd --regex "gitlab-ci.yml|gitlab-ci.staging.*"  --hidden --print0 | xargs -0 sed -i "" -e "s/ets-prod-v1.8.47-RC[0-9]\{1,\}$/$TAG/" &&
     gd
 ' Enter
-tmux send-keys -t 'k8s:SUNSTAG' '
+tmux send-keys -t 'k8s_stag :SUNSTAG' '
     open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/sun-entertainment/-/pipelines" &&
     cd ~/dev/sun-entertainment &&
     gc main &&
@@ -63,7 +63,7 @@ tmux send-keys -t 'k8s:SUNSTAG' '
     fd "gitlab*" --hidden --print0 | xargs -0 sed -i "" -e "s/ets-prod-v1.8.47-RC[0-9]\{1,\}$/$TAG/" &&
     gd
 ' Enter
-tmux send-keys -t 'k8s:ZIPSTAG' '
+tmux send-keys -t 'k8s_stag :ZIPSTAG' '
 open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/zipcity/-/pipelines" &&
     cd ~/dev/zipcity &&
     gc main &&
@@ -71,7 +71,7 @@ open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/zipcity/-/pipeline
     fd "gitlab*" --hidden --print0 | xargs -0 sed -i "" -e "s/ets-prod-v1.8.47-RC[0-9]\{1,\}$/$TAG/" &&
     gd
 ' Enter
-tmux send-keys -t 'k8s:ZKTSTAG' '
+tmux send-keys -t 'k8s_stag :ZKTSTAG' '
     open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/zicket/-/pipelines" &&
     cd ~/dev/zicket &&
     gc main &&
@@ -81,5 +81,5 @@ tmux send-keys -t 'k8s:ZKTSTAG' '
 ' Enter
 
 # Activate main window
-tmux select-window -t 'k8s:GTSSTAG'
-tmux -u attach-session -t k8s
+tmux select-window -t 'k8s_stag :GTSSTAG'
+tmux -u attach-session -t k8s_stag
