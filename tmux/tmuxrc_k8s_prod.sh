e7784e45de7a86015e60d99da14b8d3ae4b4728f
Ranel Padon <ranel.padon@gmail.com>
2021-10-01 05:00:06 +0800

    Update the Tmux - k8s Prod scripts.

--- tmux/tmuxrc_k8s_prod.sh
+++ tmux/tmuxrc_k8s_prod.sh
@@ -7,11 +7,17 @@
 # Tabs
 tmux new-session -s k8s_prod -n 'DEMOSTAG' -d
 tmux new-window -t k8s_prod -n 'GTSPROD'
+tmux new-window -t k8s_prod -n 'HKILFPROD'
+tmux new-window -t k8s_prod -n 'HKRUPROD'
 tmux new-window -t k8s_prod -n 'KGGPROD'
 tmux new-window -t k8s_prod -n 'MGMPROD'
+tmux new-window -t k8s_prod -n 'SUNPROD'  # BYPROD
 tmux new-window -t k8s_prod -n 'ZIPPROD'
+tmux new-window -t k8s_prod -n 'ZUNIPROD'
 
 # Auto-runs
+# https://git.hk.asiaticketing.com/technology/team/-/issues/243
+# DEMOSTAG is treated as PROD (has no STAG env), but deployment should still use the STAG entry.
 tmux send-keys -t 'k8s_prod:DEMOSTAG' '
     open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/demo/-/pipelines" &&
     cd ~/dev/demo &&
@@ -27,6 +33,22 @@ tmux send-keys -t 'k8s_prod:GTSPROD' '
     gpl &&
     git tag ets-prod-v1.8.47-RC$TAG &&
     git push origin ets-prod-v1.8.47-RC$TAG
+' Enter
+tmux send-keys -t 'k8s_prod:HKILFPROD' '
+    open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/hkilf/-/pipelines" &&
+    cd ~/dev/hkilf &&
+    gc main &&
+    gpl &&
+    fd --regex "gitlab-ci.yml|gitlab-ci.prod.*" --hidden --print0 | xargs -0 sed -i "" -e "s/ets-prod-v1.8.47-RC[^\s]\{1,\}$/ets-prod-v1.8.47-RC$TAG/" &&
+    gd
+' Enter
+tmux send-keys -t 'k8s_prod:HKRUPROD' '
+    open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/hkru/-/pipelines" &&
+    cd ~/dev/hkru &&
+    gc main &&
+    gpl &&
+    fd --regex "gitlab-ci.yml|gitlab-ci.prod.*" --hidden --print0 | xargs -0 sed -i "" -e "s/ets-prod-v1.8.47-RC[^\s]\{1,\}$/ets-prod-v1.8.47-RC$TAG/" &&
+    gd
 ' Enter
 tmux send-keys -t 'k8s_prod:KGGPROD' '
     open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/kgg-kg/-/pipelines" &&
@@ -44,13 +66,29 @@ tmux send-keys -t 'k8s_prod:MGMPROD' '
     fd --regex "gitlab-ci.yml|gitlab-ci.production-shared.*" --hidden --print0 | xargs -0 sed -i "" -e "s/ets-prod-v1.8.47-RC[^\s]\{1,\}$/ets-prod-v1.8.47-RC$TAG/" &&
     gd
 ' Enter
+tmux send-keys -t 'k8s_prod:SUNPROD' '
+open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/sun-entertainment/-/pipelines" &&
+    cd ~/dev/sun-entertainment &&
+    gc main &&
+    gpl &&
+    fd --regex "gitlab-ci.yml|gitlab-ci.production.*" --hidden --print0 | xargs -0 sed -i "" -e "s/ets-prod-v1.8.47-RC[^\s]\{1,\}$/ets-prod-v1.8.47-RC$TAG/" &&
+    gd
+' Enter
 tmux send-keys -t 'k8s_prod:ZIPPROD' '
 open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/zipcity/-/pipelines" &&
     cd ~/dev/zipcity &&
     gc main &&
     gpl &&
-    git tag ets-prod-v1.8.47-RC$TAG &&
-    git push origin ets-prod-v1.8.47-RC$TAG
+    fd --regex "gitlab-ci.yml|gitlab-ci.production-shared.*" --hidden --print0 | xargs -0 sed -i "" -e "s/ets-prod-v1.8.47-RC[^\s]\{1,\}$/ets-prod-v1.8.47-RC$TAG/" &&
+    gd
+' Enter
+tmux send-keys -t 'k8s_prod:ZUNIPROD' '
+open "https://git.hk.asiaticketing.com/ticketflap/whitelabels/zuni/-/pipelines" &&
+    cd ~/dev/zuni &&
+    gc main &&
+    gpl &&
+    fd --regex "gitlab-ci.yml|gitlab-ci.production.*" --hidden --print0 | xargs -0 sed -i "" -e "s/ets-prod-v1.8.47-RC[^\s]\{1,\}$/ets-prod-v1.8.47-RC$TAG/" &&
+    gd
 ' Enter
 
 # Activate main window
