6f3fc9d02aadc5ff251be4fa1f853fa56ca07004
Ranel Padon <ranel.padon@gmail.com>
2021-12-12 14:00:11 +0800

    Add the php.sh

---
+++ zshrc/php.sh
@@ -0,0 +1,9 @@
+export DRUSH_PHP=/Applications/XAMPP/xamppfiles/bin/php
+
+alias drush="php /Users/ranelpadon/dev/drush7/drush.php"
+alias ds='drush status'
+
+# alias na='ssh -t e35d18971@new.appledrafting.com "cd public_html && bash -l"'
+
+# Auto-completion for Drupal Console
+source "$HOME/.console/console.rc" 2>/dev/null
