599aa5ed366984b547c9f1d8486ed1a3c9e9c8f7
Ranel Padon <ranel.padon@gmail.com>
2021-12-12 14:00:23 +0800

    Add the macos.sh

---
+++ zshrc/macos.sh
@@ -0,0 +1,7 @@
+export XDG_CACHE_HOME=/Users/ranelpadon/.cache
+
+# Show/hide Apple's hidden files.
+alias files_show='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias files_hide='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+alias xcode='open -a Xcode'
