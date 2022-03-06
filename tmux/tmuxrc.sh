6d3ac960bdd816b9b9af00fb4f08f8078cc3422e
Ranel Padon <ranel.padon@gmail.com>
2022-03-06 22:53:55 +0800

    Update the tmuxrc.sh

--- tmux/tmuxrc.sh
+++ tmux/tmuxrc.sh
@@ -4,7 +4,9 @@
 tmux new-session -s Workspace -n "Configs" -d
 tmux new-window -t Workspace -n "Vortex"
 tmux new-window -t Workspace -n "Nvim ETS"
-tmux new-window -t Workspace -n "ETS"
+tmux new-window -t Workspace -n "ETS-1"
+tmux new-window -t Workspace -n "ETS-2"
+tmux new-window -t Workspace -n "ETS-3"
 tmux new-window -t Workspace -n "Docker"
 tmux new-window -t Workspace -n "Dev"
 tmux new-window -t Workspace -n "Home"
@@ -13,8 +15,9 @@ tmux new-window -t Workspace -n "Home"
 tmux send-keys -t "Workspace:Configs" "cd ~/dev/configs && nvim" Enter
 tmux send-keys -t "Workspace:Vortex" "cd ~/Dropbox/Vortex && nvim" Enter
 tmux send-keys -t "Workspace:Nvim ETS" "cd ~/dev/ticketflap/ticketing && pat && nvim" Enter
-tmux send-keys -t "Workspace:ETS" "cd ~/dev/ticketflap/ticketing" Enter
-tmux send-keys -t "Workspace:Docker" "cd ~/dev/ticketflap/ticketing/docker" Enter
+tmux send-keys -t "Workspace:ETS-1" "cd ~/dev/ticketflap/ticketing && pat" Enter
+tmux send-keys -t "Workspace:ETS-2" "cd ~/dev/ticketflap/ticketing && pat" Enter
+tmux send-keys -t "Workspace:ETS-3" "cd ~/dev/ticketflap/ticketing && pat" Enter
 tmux send-keys -t "Workspace:Dev" "cd ~/dev" Enter
 
 # Activate main window
