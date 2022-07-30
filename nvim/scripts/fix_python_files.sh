9d629a3b62bc9e0c859649d056aaa1d783ef1ed5
Ranel Padon <ranel.padon@gmail.com>
2022-07-31 04:01:20 +0800

    Add the fix_python_files.sh script.

---
+++ nvim/scripts/fix_python_files.sh
@@ -0,0 +1,19 @@
+# Custom command combining `isort` and `autoflake`.
+
+# zsh aliases will not work in Vim, so need to redefine the shell function.
+# https://unix.stackexchange.com/questions/550622/how-to-run-aliases-commands-in-inside-the-vim-editor
+fix_python_files() {
+    modified_files=$(git diff --name-only | grep '.py' | grep -v 'migrations/' | grep -v 'conf/')
+    files=$modified_files
+
+    # No need inside of Vim, since the files are separated by spaces, not newlines.
+    # Enabling this will cause some logic error.
+    # files=($(echo $modified_files | tr '\n' ' '))
+
+    for file in $files; do
+        isort $file
+        autoflake $file --remove-all-unused-imports --in-place --exclude 'conf/settings/*'
+    done
+}
+
+fix_python_files
