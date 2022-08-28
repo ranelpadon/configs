dd012c67fcbae39f923c30df668cba7f1477ff34
Ranel Padon <ranel.padon@gmail.com>
2022-08-28 08:20:21 +0800

    Update the tmuxrc.sh.

--- tmux/tmuxrc.sh
+++ tmux/tmuxrc.sh
@@ -12,8 +12,8 @@ tmux new-window -t Workspace -n "Dev"
 tmux new-window -t Workspace -n "Home"
 
 # Auto-runs
-tmux send-keys -t "Workspace:Configs" "cd ~/dev/configs && nvim" Enter
-tmux send-keys -t "Workspace:Vortex" "cd ~/Dropbox/Vortex && nvim" Enter
+tmux send-keys -t "Workspace:Configs" "cd ~/dev/configs && pat && nvim" Enter
+tmux send-keys -t "Workspace:Vortex" "cd ~/Dropbox/Vortex && pat && nvim" Enter
 tmux send-keys -t "Workspace:Nvim ETS" "cd ~/dev/ticketflap/ticketing && pat && nvim" Enter
 tmux send-keys -t "Workspace:ETS-1" "cd ~/dev/ticketflap/ticketing && pat" Enter
 tmux send-keys -t "Workspace:ETS-2" "cd ~/dev/ticketflap/ticketing && pat" Enter
