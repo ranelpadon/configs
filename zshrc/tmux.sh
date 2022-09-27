51292400a9e3c48edcb41dc76e8dd1090932bf16
Ranel Padon <ranel.padon@gmail.com>
2022-09-27 18:18:44 +0800

    Update the tmux.sh.

--- zshrc/tmux.sh
+++ zshrc/tmux.sh
@@ -3,9 +3,9 @@ _tmuxx_resume() {
 }
 
 alias tmuxx='source ~/.config/tmux/tmuxrc.sh'
-alias tmuxx_kube_test='source ~/.config/tmux/tmuxrc_k8s_test.sh'
-alias tmuxx_kube_stag='source ~/.config/tmux/tmuxrc_k8s_stag.sh'
-alias tmuxx_kube_prod='source ~/.config/tmux/tmuxrc_k8s_prod.sh'
+alias tmuxx_test='source ~/.config/tmux/tmuxrc_k8s_test.sh'
+alias tmuxx_stag='source ~/.config/tmux/tmuxrc_k8s_stag.sh'
+alias tmuxx_prod='source ~/.config/tmux/tmuxrc_k8s_prod.sh'
 
 alias tmuxx_resume='_tmuxx_resume Workspace'
 alias tmuxx_resume_test='_tmuxx_resume k8s_test'
@@ -18,26 +18,28 @@ alias tmuxx_resume_prod='_tmuxx_resume k8s_prod'
 update_ets_version() {
     tmux set-environment -g TAG $2
     ENV=$(echo $1 | tr '[:upper:]' '[:lower:]')
-    eval tmuxx_kube_$ENV
+    eval tmuxx_$ENV
 }
 
-# commit_ets_version TEST 123
-# commit_ets_version STAG 123
-# commit_ets_version PROD 123
-commit_ets_version() {
+# commit_ets_version "Update STAG ETS Queue version to 1.0.7."
+tmux_commit_message() {
     # Send the same Git commit message to all active windows.
     SESSION_NAME=$(tmux display-message -p '#S')
 
-    if [[ $1 == "TEST" ]]
-    then
-        MESSAGE="
-            gca 'Update $1 to ets-test-v3.0.$2.'
-        "
-    else
-        MESSAGE="
-            gca 'Update $1 to ets-prod-v2.0.$2.'
-        "
-    fi
+    MESSAGE="
+        gca '$1'
+    "
+
+    # if [[ $1 == "TEST" ]]
+    # then
+    #     MESSAGE="
+    #         gca 'Update $1 to ets-test-v3.0.$2.'
+    #     "
+    # else
+    #     MESSAGE="
+    #         gca 'Update $1 to ets-prod-v2.0.$2.'
+    #     "
+    # fi
 
     MESSAGE+='gps'
 
