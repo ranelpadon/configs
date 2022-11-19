25a8eab179c20bcada5cfa8c2e24e650eab8e2c3
Ranel Padon <ranel.padon@gmail.com>
2022-11-19 12:01:09 +0800

    Update the tmuxrc.sh.

--- tmux/tmuxrc.sh
+++ tmux/tmuxrc.sh
@@ -7,7 +7,9 @@ tmux new-window -t Workspace -n "Nvim ETS"
 tmux new-window -t Workspace -n "ETS-1"
 tmux new-window -t Workspace -n "ETS-2"
 tmux new-window -t Workspace -n "ETS-3"
-tmux new-window -t Workspace -n "Dev"
+tmux new-window -t Workspace -n "Whitelabels"
+tmux new-window -t Workspace -n "Django"
+tmux new-window -t Workspace -n "Desktop"
 
 # Auto-runs
 tmux send-keys -t "Workspace:Configs" "cd ~/dev/configs && nvim" Enter
@@ -16,7 +18,9 @@ tmux send-keys -t "Workspace:Nvim ETS" "cd ~/dev/ticketflap/ticketing && pat && 
 tmux send-keys -t "Workspace:ETS-1" "cd ~/dev/ticketflap/ticketing && pat" Enter
 tmux send-keys -t "Workspace:ETS-2" "cd ~/dev/ticketflap/ticketing && pat" Enter
 tmux send-keys -t "Workspace:ETS-3" "cd ~/dev/ticketflap/ticketing && pat" Enter
-tmux send-keys -t "Workspace:Dev" "cd ~/dev" Enter
+tmux send-keys -t "Workspace:Whitelabels" "cd ~/dev/whitelabels/scripts/python" Enter
+tmux send-keys -t "Workspace:Django" "cd /opt/homebrew/Caskroom/miniforge/base/envs/ticketing/lib/python2.7/site-packages/django" Enter
+tmux send-keys -t "Workspace:Desktop" "cd ~/Desktop" Enter
 
 # Activate main window
 tmux select-window -t "Workspace:Configs"
